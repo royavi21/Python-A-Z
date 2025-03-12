a770b4282f1d2c14049eb43f52db4e3b6a1c65ce
Avijit Roy <aviroy2005@gmail.com>
2025-03-13 01:21:29 +0600

    4th commit

--- Chapter -3/Chapter -3 Problem_solve/p2.py
+++ Chapter -3/Chapter -3 Problem_solve/p2.py
@@ -1,1 +1,9 @@
-#Problem set 2
+# Write a program to fill in a letter template given below with name and date. 
+
+# letter = '''
+            # Dear <|Name|>,
+            # You are selected!
+            # <|Date|>
+        # '''
+
+
