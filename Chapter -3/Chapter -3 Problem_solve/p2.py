6adb37f5b2ec453f36ba3cc733c589e274fd8ea2
Avijit Roy <aviroy2005@gmail.com>
2025-03-13 22:32:17 +0600

    5th commit

--- Chapter -3/Chapter -3 Problem_solve/p2.py
+++ Chapter -3/Chapter -3 Problem_solve/p2.py
@@ -1,9 +1,7 @@
 # Write a program to fill in a letter template given below with name and date. 
 
-# letter = '''
-            # Dear <|Name|>,
-            # You are selected!
-            # <|Date|>
-        # '''
-
-
+letter = '''Dear <|Name|>,
+You are selected!
+<|Date|>
+'''
+print(letter.replace("<|Name|>", "Avijit") .replace("<|Date|>", "24th October 2025"))
