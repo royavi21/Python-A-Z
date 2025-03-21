50d5e9aaa73ca10cfff5d880631da1e9e4097ced
Avijit Roy <aviroy2005@gmail.com>
2025-03-21 20:01:17 +0600

    21th commit

---
+++ Chapter -6/Chapter -6 problem_solve/p7.py
@@ -0,0 +1,12 @@
+#  Write a program to find out whether a given post is talking about “intel” or not.
+
+# post = "I'm suing a intel arc a750 gpu."
+
+post=input("Write about your post: ")
+
+if ("intel" in post):
+    print("intel word is in the post")
+    
+
+else:
+    print("intel word is not in the post")
