05c5538da35d44c52c648c9345d9fa2168721328
Avijit Roy <aviroy2005@gmail.com>
2025-03-27 22:26:42 +0600

    29th commit

---
+++ Chapter -7/01_loops.py
@@ -0,0 +1,18 @@
+# CHAPTER 7 – LOOPS IN PYTHON 
+
+"""import pyautogui
+import time
+while True:
+    time.sleep(1)
+    pyautogui.typewrite("Hola man")
+    pyautogui.press('enter')"""
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+#The same task can be done by loops, and that's very easy.
+for i in range (1,6):
+    print(i)
--- Chapter -7/01_text.py
+++
@@ -1,8 +0,0 @@
-# CHAPTER 7 – LOOPS IN PYTHON 
-
-import pyautogui
-import time
-while True:
-    time.sleep(1)
-    pyautogui.typewrite("Hola man")
-    pyautogui.press('enter')
