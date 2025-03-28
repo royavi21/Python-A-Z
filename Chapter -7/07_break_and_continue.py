e2ccc917e5c524b3dc897598708e518bc5b4e3b3
Avijit Roy <aviroy2005@gmail.com>
2025-03-28 22:43:20 +0600

    32th commit

---
+++ Chapter -7/07_break_and_continue.py
@@ -0,0 +1,11 @@
+# Break and continue
+
+for i in range(50):
+    if i ==19:
+        break   #Exit the loop right now. ❌
+    print(i)
+
+for i in range(30):
+    if i ==19:
+        continue   #Skip the iteration right now. ❌
+    print(i)
