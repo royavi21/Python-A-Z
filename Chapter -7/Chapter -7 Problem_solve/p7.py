0d0bac9ffec86b986695c550dbb5fe93d004d755
Avijit Roy <aviroy2005@gmail.com>
2025-03-29 11:56:13 +0600

    36th commit

--- Chapter -7/Chapter -7 Problem_solve/p7.py
+++ Chapter -7/Chapter -7 Problem_solve/p7.py
@@ -1,7 +1,21 @@
 '''Write a program to print the following star pattern.
-*
-***
+  *
+ ***
 ***** for n = 3 '''
 
-for i in range(1,4):
-    print("*"*i)
+n = int(input("Enter the number of rows: "))
+for i in range(1, n+1):
+    print(" " * (n-i), end="")
+    print("*" * (2*i-1), end="")
+    print()
+
+''' 
+Output:
+
+    *
+   ***
+  *****
+ *******
+*********
+
+'''
