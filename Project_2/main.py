21fbd1fcf85502b735b80e711bcd067a1fa90d23
Avijit Roy <aviroy2005@gmail.com>
2025-10-07 21:09:57 +0600

    Guess game

--- Project_2/main.py
+++ Project_2/main.py
@@ -1,14 +1,15 @@
 import random
 n=random.randint(1,100)
 a = -1
-guesses = 0
+guesses = 1
 while (a != n):
-    guesses +=1
     a=int(input("Guess the number: "))
     
     if (a>n):
         print("Lower number please")
+        guesses +=1
     else:
         print("Higher number please")
+        guesses +=1
         
-print(f"You guessed the number {n} in {guesses} guesses attempt")
+print(f"You guessed the number {n} in ({guesses}) guesses attempt")
